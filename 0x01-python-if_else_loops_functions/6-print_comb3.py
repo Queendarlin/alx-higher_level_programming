28f487020910dee0ee71a32a15d3747ecfe338d2
Queendarlin <queendarlinnnamani@gmail.com>
2023-11-28 11:18:49 -0800

    A program that prints all possible different combinations of two digits

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for tens_num in range(9):
+    for ones_num in range(tens_num + 1, 10):
+        if tens_num * 10 + ones_num < 89:
+            print("{:d}{:d}".format(tens_num, ones_num), end=", ")
+print("{:d}".format(89))
