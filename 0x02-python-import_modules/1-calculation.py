3dc5ed9caf6f31a6218fadbc48956f1d765a37e9
Queendarlin <queendarlinnnamani@gmail.com>
2023-11-30 10:25:04 -0800

    A program that imports maths calculation functions

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+if __name__ == "__main__":
+
+    a = 10
+    b = 5
+
+    add_calc = add(a, b)
+    sub_calc = sub(a, b)
+    mul_calc = mul(a, b)
+    div_calc = div(a, b)
+
+    print("{} + {} = {}".format(a, b, add_calc))
+    print("{} - {} = {}".format(a, b, sub_calc))
+    print("{} * {} = {}".format(a, b, mul_calc))
+    print("{} / {} = {}".format(a, b, div_calc))
