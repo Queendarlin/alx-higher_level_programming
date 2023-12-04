12567853c99cd6d9ce4f8e66ef875b154a807dfe
Queendarlin <queendarlinnnamani@gmail.com>
2023-12-03 20:32:49 -0800

    A function in C that checks if a singly linked list is a palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,44 +1,67 @@
 #include "lists.h"
 
 /**
- * is_palindrome - checks if a singly linked list is a palindrome
- * @head: pointer to the head of the linked list
+ * is_palindrome - Find out if a singly linked list is a palindrome
+ * @head: pointer to the head
  * Return: 1 if it is a palindrome, else 0
  */
 int is_palindrome(listint_t **head)
 {
+	listint_t *temp, *reverse, *mid_ptr;
+	size_t size = 0, index;
+
 	if (*head == NULL || (*head)->next == NULL)
 		return (1);
 
-	listint_t *slow_ptr = *head;
-	listint_t *fast_ptr = *head;
-	listint_t *prev_slow = NULL;
-	listint_t *second_half = NULL;
-	listint_t *temp;
-
-	while (fast_ptr != NULL && fast_ptr->next != NULL)
+	temp = *head;
+	while (temp)
 	{
-		fast_ptr = fast_ptr->next->next;
-
-		temp = slow_ptr->next;
-		slow_ptr->next = prev_slow;
-		prev_slow = slow_ptr;
-		slow_ptr = temp;
+		size++;
+		temp = temp->next;
 	}
 
-	if (fast_ptr != NULL)
-		slow_ptr = slow_ptr->next;
+	temp = *head;
+	for (index = 0; index < (size / 2) - 1; index++)
+		temp = temp->next;
 
-	second_half = slow_ptr;
+	if ((size % 2) == 0 && temp->n != temp->next->n)
+		return (0);
 
-	while (second_half != NULL)
+	temp = temp->next->next;
+	reverse = reverse_listint(&temp);
+	mid_ptr = reverse;
+
+	temp = *head;
+	while (reverse)
 	{
-		if (prev_slow->n != second_half->n)
+		if (temp->n != reverse->n)
 			return (0);
-
-		prev_slow = prev_slow->next;
-		second_half = second_half->next;
+		temp = temp->next;
+		reverse = reverse->next;
 	}
+	reverse_listint(&mid_ptr);
 
 	return (1);
+}
+
+/**
+ * reverse_listint - Function to reverse the singly-linked list.
+ * @head: A pointer to head.
+ *
+ * Return: The pointer to the head
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *node_ptr = *head, *next, *previous_ptr = NULL;
+
+	while (node_ptr)
+	{
+		next = node_ptr->next;
+		node_ptr->next = previous_ptr;
+		previous_ptr = node_ptr;
+		node_ptr = next;
+	}
+
+	*head = previous_ptr;
+	return (*head);
 }
