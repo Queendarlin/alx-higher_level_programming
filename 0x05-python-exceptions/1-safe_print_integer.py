09e63a7aeea69d17ea306790ab7a38ff685d27cd
Queendarlin <queendarlinnnamani@gmail.com>
2023-12-18 04:48:13 -0800

    A function that prints an integer

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,9 @@
+#!/bin/python3
+
+def safe_print_integer(value):
+    try:
+        if isinstance(value, int):
+            print("{:d}".format(value))
+            return True
+    except (ValueError, TypeError):
+        return False
