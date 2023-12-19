6e65ce6fa8ca9cd1d2b1c63bf6151167544f5ecf
Queendarlin <queendarlinnnamani@gmail.com>
2023-12-19 15:49:43 -0800

    Print square instance

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -57,9 +57,11 @@ class Square:
 
     def __str__(self):
         """Returns a string representation of the square"""
-        result = []
-        for index in range(self.__position[1]):
-            result.append('')
-        for index in range(self.__size):
-            result.append(" " * self.__position[0] + "#" * self.__size)
-        return '\n'.join(result).replace(" ", "_")
+        if self.__size != 0:
+            [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            if i != self.__size - 1:
+                print("")
+        return ("")
