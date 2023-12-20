6e1e173d96ed6ee43245424bb0842dc3836a41cb
Queendarlin <queendarlinnnamani@gmail.com>
2023-12-19 16:03:01 -0800

    Python class MagicClass

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import math
+
+
+class MagicClass:
+    """Python class MagicClass equivalent to given bytecode."""
+
+    def __init__(self, radius=0):
+        """Initialize MagicClass instance with radius."""
+        self.__radius = 0
+
+        if type(radius) is not int or type(radius) is not float:
+            raise TypeError('radius must be a number')
+
+        self.__radius = radius
+
+    def area(self):
+        """Calculate and return the area."""
+        return self.__radius ** 2 * math.pi
+
+    def circumference(self):
+        """Calculate and return the circumference."""
+        return 2 * math.pi * self.__radius
