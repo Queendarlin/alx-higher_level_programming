82bbaa3578ce7347db700d3c3776d19ebc1d7f8b
Queendarlin <queendarlinnnamani@gmail.com>
2024-01-05 08:10:45 -0800

    A a class LockedClass with no class or object attribute

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+"""
+A class LockedClass
+"""
+
+
 class LockedClass:
+    """To prevent user from dynamically creating new instance attributes"""
     __slots__ = ('first_name')
