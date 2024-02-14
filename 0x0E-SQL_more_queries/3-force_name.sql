74b2202db1d5e59e916ef3f8acc756ca7fc66ee2
Queendarlin <queendarlinnnamani@gmail.com>
2024-02-14 10:57:35 -0800

    Always a name

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,4 +1,5 @@
 -- A script that creates the table force_name on your MySQL server.
+-- Create the table force_name if it doesn't exist
 CREATE TABLE IF NOT EXISTS force_name (
 	id INT,
 	name VARCHAR(256) NOT NULL
