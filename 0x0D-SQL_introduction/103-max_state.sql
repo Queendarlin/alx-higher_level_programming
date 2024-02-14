49c10d04142cceec073519d539c2eedf7e048a51
Queendarlin <queendarlinnnamani@gmail.com>
2024-02-13 23:50:04 -0800

    Avg

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,6 +1,5 @@
 -- To display the maximum temperature of each state ordered by state name
-USE hbtn_0c_0;
-SELECT state, MAX(temperature) AS max_temp
+SELECT state, MAX(value) AS max_temp
 FROM temperatures
 GROUP BY state
-ORDER BY name;
+ORDER BY state;
