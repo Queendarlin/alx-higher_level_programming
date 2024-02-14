3516f5593571a580c930060e0213438741226300
Queendarlin <queendarlinnnamani@gmail.com>
2024-02-14 12:56:53 -0800

    Rotten tomatoes

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -5,4 +5,4 @@ SELECT title, SUM(tv_show_ratings.rate) 'rating'
 FROM tv_shows
 LEFT JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
 GROUP BY tv_shows.title
-ORDER BY rating_sum DESC;
+ORDER BY rating DESC;
