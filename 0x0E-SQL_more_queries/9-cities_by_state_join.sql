23c05dfa3be93b11af91d0ae23bb8bc694d2473c
Queendarlin <queendarlinnnamani@gmail.com>
2024-02-14 12:20:05 -0800

    Cities by States

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,7 +1,7 @@
 -- A script that lists all cities contained in the database hbtn_0d_usa
 
 -- Select cities and corresponding state names using JOIN
-SELECT id, name, name
+SELECT cities.id, cities.name, states.name
 FROM cities
 JOIN states ON cities.state_id = states.id
-ORDER BY id ASC;
+ORDER BY cities.id ASC;
