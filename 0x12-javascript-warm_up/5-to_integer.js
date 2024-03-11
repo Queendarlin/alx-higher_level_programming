79d04c0a896a76c784d28ba8d0c342a783e4da88
Queendarlin <queendarlinnnamani@gmail.com>
2024-03-11 12:47:05 -0700

    Task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+
+// Get the first argument passed to the script
+const arg = process.argv[2];
+
+// Convert the argument to an integer using parseInt() and check if it's a valid integer
+const number = parseInt(arg);
+
+// Check if the conversion is successful and print the output accordingly
+if (!isNaN(number)) {
+  console.log(`My number: ${number}`);
+} else {
+  console.log('Not a number');
+}
