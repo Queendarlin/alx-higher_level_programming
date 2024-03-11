6d7ed96dbd4e0a0fe044908415074c50b2f36066
Queendarlin <queendarlinnnamani@gmail.com>
2024-03-11 16:58:55 -0700

    Object function property

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,1 +1,25 @@
 #!/usr/bin/node
+
+// Define the myObject object
+const myObject = {
+  type: 'object',
+  value: 12,
+  // Define the incr function
+  incr: function () {
+    this.value++;
+  }
+};
+
+console.log(myObject);
+
+// Call incr function to increment the value
+myObject.incr();
+console.log(myObject);
+
+// Call incr function again
+myObject.incr();
+console.log(myObject);
+
+// Call incr function again
+myObject.incr();
+console.log(myObject);
