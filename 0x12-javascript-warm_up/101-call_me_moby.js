fe19f3a872a1f761c15b203ae93b28a24ed8bc3e
Queendarlin <queendarlinnnamani@gmail.com>
2024-03-11 16:55:06 -0700

    Tasks

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+// Define the callMeMoby function
+function callMeMoby (x, theFunction) {
+  for (let i = 0; i < x; i++) {
+    theFunction();
+  }
+}
+
+// Export the callMeMoby function to make it visible from outside
+module.exports.callMeMoby = callMeMoby;
--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+
+// Define the addMeMaybe function
+function addMeMaybe (number, theFunction) {
+  // Increment the number by 1
+  number += 1;
+  // Call theFunction with the incremented number as argument
+  theFunction(number);
+}
+
+// Export the addMeMaybe function to make it visible from outside
+module.exports.addMeMaybe = addMeMaybe;
