1b070e0e339fb5bf71379d3db55cdd082b888d69
Queendarlin <queendarlinnnamani@gmail.com>
2024-03-11 16:12:45 -0700

    Task7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -9,9 +9,11 @@ const numOccurrences = parseInt(arg);
 // Check if the conversion is successful and if it's a positive integer
 if (!isNaN(numOccurrences) && numOccurrences > 0) {
   // Loop to print "C is fun" numOccurrences times
+  let output = '';
   for (let i = 0; i < numOccurrences; i++) {
-    console.log('C is fun');
+    output += 'C is fun\n';
   }
+  console.log(output.trim()); // Print the output after removing the trailing newline
 } else {
   console.log('Missing number of occurrences');
 }
