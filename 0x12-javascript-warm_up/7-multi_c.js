7f1e37e641bfd8c1f3b0aa561714ca052527bb98
Queendarlin <queendarlinnnamani@gmail.com>
2024-03-11 16:17:56 -0700

    Task7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,19 +1,15 @@
 #!/usr/bin/node
 
-// Get the first argument passed to the script
-const arg = process.argv[2];
-
-// Convert the argument to an integer using parseInt()
-const numOccurrences = parseInt(arg);
-
-// Check if the conversion is successful and if it's a positive integer
-if (!isNaN(numOccurrences) && numOccurrences > 0) {
-  // Loop to print "C is fun" numOccurrences times
-  let output = '';
-  for (let i = 0; i < numOccurrences; i++) {
-    output += 'C is fun\n';
-  }
-  console.log(output.trim()); // Print the output after removing the trailing newline
-} else {
+// Check if the first argument is missing or not a number
+if (process.argv[2] === undefined || isNaN(process.argv[2])) {
   console.log('Missing number of occurrences');
+} else {
+  const x = Number(process.argv[2]); // Convert the argument to a number
+  let i = 0;
+  
+  // Loop to print "C is fun" x times
+  while (i < x) {
+    console.log('C is fun');
+    i++;
+  }
 }
