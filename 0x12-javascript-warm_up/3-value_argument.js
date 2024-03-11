bd29c051463d1be900c6d0e20ce6177a88015447
Queendarlin <queendarlinnnamani@gmail.com>
2024-03-11 12:31:38 -0700

    Task3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+// Get the first argument passed to the script
+const arg = process.argv[2];
+
+// Check if an argument is provided and print it, else print "No argument"
+if (arg !== undefined) {
+  console.log(arg);
+} else {
+  console.log('No argument');
+}
