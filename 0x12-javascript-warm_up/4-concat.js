d7c45f7fe3eca67d622bc2990bd25fade8a60f14
Queendarlin <queendarlinnnamani@gmail.com>
2024-03-11 12:38:31 -0700

    Task4

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+
+// Get the first and second arguments passed to the script
+const arg1 = process.argv[2] || 'undefined'; // If no argument provided, set to 'undefined'
+const arg2 = process.argv[3] || 'undefined'; // If no second argument provided, set to 'undefined'
+
+// Print the arguments in the specified format
+console.log(`${arg1} is ${arg2}`);
