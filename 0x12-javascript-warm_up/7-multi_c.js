f954367c7fa642a32606051668ad216a1a3b6664
Queendarlin <queendarlinnnamani@gmail.com>
2024-03-11 16:20:03 -0700

    Task7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -6,7 +6,7 @@ if (process.argv[2] === undefined || isNaN(process.argv[2])) {
 } else {
   const x = Number(process.argv[2]); // Convert the argument to a number
   let i = 0;
-  
+
   // Loop to print "C is fun" x times
   while (i < x) {
     console.log('C is fun');
