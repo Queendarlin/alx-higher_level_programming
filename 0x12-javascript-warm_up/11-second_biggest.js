82187fea555aa0558810c205a66e3185b47d5b0f
Queendarlin <queendarlinnnamani@gmail.com>
2024-03-11 16:37:11 -0700

    Second biggest number

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+
+// Get the arguments passed to the script
+const args = process.argv.slice(2);
+
+// Convert arguments to integers and sort them in descending order
+const integers = args.map(Number).sort((a, b) => b - a);
+
+// Check if no arguments are passed or only one argument is passed
+if (integers.length === 0 || integers.length === 1) {
+  console.log(0);
+} else {
+  // Print the second biggest integer
+  console.log(integers[1]);
+}
