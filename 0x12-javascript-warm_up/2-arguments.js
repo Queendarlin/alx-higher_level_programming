e43874b8c6f2d070ec60359c1d3e0dc4c84fce11
Queendarlin <queendarlinnnamani@gmail.com>
2024-03-11 12:23:14 -0700

    process argv

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-numArgs = process.argv.length -2;
+const numArgs = process.argv.length - 2;
 if (numArgs === 0) {
-    console.log('No argument');
+  console.log('No argument');
 } else if (numArgs === 1) {
-    console.log('Argument found');
+  console.log('Argument found');
 } else {
-    console.log('Arguments found');
+  console.log('Arguments found');
 }
