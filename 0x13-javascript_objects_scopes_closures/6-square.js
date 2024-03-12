0a0e114ef2aa008bd66a0b6af367b7f51eaadbf9
Queendarlin <queendarlinnnamani@gmail.com>
2024-03-12 12:59:18 -0700

    Square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+
+const SquareSuper = require('./5-square');
+
+class Square extends SquareSuper {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
