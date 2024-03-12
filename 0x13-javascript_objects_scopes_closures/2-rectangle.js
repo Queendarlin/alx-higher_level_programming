db028296fd43882fd15f1981838fa47723ed6d55
Queendarlin <queendarlinnnamani@gmail.com>
2024-03-12 12:33:00 -0700

    Tasks

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,9 +2,7 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0) {
-      return {}; // Return an empty object if width or height is not a positive integer
-    } else {
+    if ((w > 0) && (h > 0)) {
       this.width = w;
       this.height = h;
     }
