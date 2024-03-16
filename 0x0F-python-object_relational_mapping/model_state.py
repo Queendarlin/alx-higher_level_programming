b22aec63d2920eb18da801964190fea525a09722
Queendarlin <queendarlinnnamani@gmail.com>
2024-03-16 11:03:43 -0700

    Using ORM

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+"""Define State class and create Base instance"""
+
+import sys
+from sqlalchemy import Table, Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+
+# Create a Base instance
+Base = declarative_base()
+
+
+class State(Base):
+    """State class linked to the MySQL table states"""
+    __tablename__ = "states"
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+
+
+if __name__ == "__main__":
+    # Create engine to connect to MySQL database
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    # Create the tables defined in Base's metadata
+    Base.metadata.create_all(engine)
