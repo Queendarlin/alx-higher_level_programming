1a6270d20e77e5d319fb55d06a254489e125d022
Queendarlin <queendarlinnnamani@gmail.com>
2024-03-15 20:13:04 -0700

    Task4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,9 +1,8 @@
-#!/usr/bin/python3
+# !/usr/bin/python3
 """Script that lists all cities from the database"""
 
 import MySQLdb
 import sys
-
 
 if __name__ == "__main__":
     """Executes the script when run directly."""
@@ -17,8 +16,14 @@ if __name__ == "__main__":
     # Create a cursor object using cursor() method
     cursor = db.cursor()
 
-    # Execute SQL query to fetch all cities
-    cursor.execute("SELECT * FROM cities ORDER BY id ASC")
+    # Execute SQL query to fetch all cities with state name
+    cursor.execute("""
+    SELECT cities.id, cities.name, states.name
+    FROM cities
+    JOIN states
+    ON cities.state_id = states.id
+    ORDER BY cities.id ASC
+    """)
 
     # Fetch all rows and print them
     rows = cursor.fetchall()
