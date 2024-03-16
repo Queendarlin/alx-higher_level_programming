7d6658c72519e58c64d5065af6f8eb22330ba121
Queendarlin <queendarlinnnamani@gmail.com>
2024-03-16 09:53:07 -0700

    Task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-"""Script that takes in the name of a state as an argument
-and lists all cities of that state"""
+"""
+Script that takes in the name of a state as an argument
+and lists all cities of that state, using the database hbtn_0e_4_usa.
+"""
 
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    """Executes the script when run directly."""
+    """
+    Connect to the database and retrieve cities based on the state name.
+    """
+
     # Extract arguments
     username, password, database, state_name = sys.argv[1:]
 
@@ -17,21 +22,23 @@ if __name__ == "__main__":
     # Create a cursor object using cursor() method
     cursor = db.cursor()
 
-    # Execute SQL query with state name as parameter
-    cursor.execute("""
-    SELECT cities.id, cities.name
+    # Prepare SQL query with parameterized query for state name
+    sql_query = """
+    SELECT GROUP_CONCAT(name SEPARATOR ', ')
     FROM cities
     JOIN states
     ON cities.state_id = states.id
-    WHERE states.name LIKE BINARY %(state_name)s
-    ORDER BY cities.id ASC
-    """, {state_name})
+    WHERE states.name LIKE BINARY %s
+    """
+
+    # Execute SQL query with state name as parameter
+    cursor.execute(sql_query, (state_name,))
 
     # Fetch the result and print it
-    result = cursor.fetchall()
+    result = cursor.fetchone()
 
     if result is not None:
-        print(", ".join([row[1] for row in result]))
+        print(result[0])
 
     # Close the cursor and Database connection
     cursor.close()
