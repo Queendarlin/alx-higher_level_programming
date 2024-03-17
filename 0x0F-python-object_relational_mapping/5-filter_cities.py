b79901874294b4fb7fd12636f2ee84afe602f2ea
Queendarlin <queendarlinnnamani@gmail.com>
2024-03-16 18:19:00 -0700

    Relationship

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -12,12 +12,9 @@ if __name__ == "__main__":
     Connect to the database and retrieve cities based on the state name.
     """
 
-    # Extract arguments
-    username, password, database, state_name = sys.argv[1:]
-
     # Connect to MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=username, passwd=password, db=database)
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3]))
 
     # Create a cursor object using cursor() method
     cursor = db.cursor()
@@ -39,7 +36,7 @@ if __name__ == "__main__":
         """, {
             'state_name': argv[4]
         })
-        result = cursor.fetchall()
+    result = cursor.fetchall()
 
     if result is not None:
         print(", ".join([row[1] for row in result]))
