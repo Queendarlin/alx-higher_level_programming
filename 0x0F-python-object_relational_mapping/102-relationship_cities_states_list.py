5d40adcb275e373147d74b65a2b1b5e32a8259c7
Queendarlin <queendarlinnnamani@gmail.com>
2024-03-16 17:58:29 -0700

    Relationship

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -8,12 +8,8 @@ from relationship_state import Base, State
 from relationship_city import City
 
 if __name__ == "__main__":
-    # Extract arguments
-    username, password, database = sys.argv[1:]
-
-    # Create engine and bind session
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
-                           .format(username, password, database))
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
