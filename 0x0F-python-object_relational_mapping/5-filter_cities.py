3106f1b12b6843c33ea69c428836b71a219b8f90
Queendarlin <queendarlinnnamani@gmail.com>
2024-03-16 18:22:16 -0700

    Relationship

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -5,42 +5,18 @@ and lists all cities of that state, using the database hbtn_0e_4_usa.
 """
 
 import MySQLdb
-from sys import argv
+import sys
+
 
 if __name__ == "__main__":
-    """
-    Connect to the database and retrieve cities based on the state name.
-    """
-
-    # Connect to MySQL server
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-        argv[1], argv[2], argv[3]))
-
-    # Create a cursor object using cursor() method
-    cursor = db.cursor()
-
-    # Prepare SQL query with parameterized query for state name
-    cursor.execute("""
-            SELECT
-                cities.id, cities.name
-            FROM
-                cities
-            JOIN
-                states
-            ON
-                cities.state_id = states.id
-            WHERE
-                states.name LIKE BINARY %(state_name)s
-            ORDER BY
-                cities.id ASC
-        """, {
-            'state_name': argv[4]
-        })
-    result = cursor.fetchall()
-
-    if result is not None:
-        print(", ".join([row[1] for row in result]))
-
-    # Close the cursor and Database connection
-    cursor.close()
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    c = db.cursor()
+    c.execute("""SELECT cities.name FROM
+                cities INNER JOIN states ON states.id=cities.state_id
+                WHERE states.name=%s""", (sys.argv[4],))
+    rows = c.fetchall()
+    tmp = list(row[0] for row in rows)
+    print(*tmp, sep=", ")
+    c.close()
     db.close()
