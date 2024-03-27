f4881c14b52ba2c08696a85f3aae233338d2d1b6
Queendarlin <queendarlinnnamani@gmail.com>
2024-03-27 12:32:37 -0700

    cURL POST parameters

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a POST request to the URL passed as an argument with email and subject parameters, and displays the body of the response
-curl -s -X POST -d "email: test@gmail.com&subject: I%20will%20always%20be%20here%20for%20PLD" "$1"
+curl -s -X POST -d "email=test@gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD" "$1"
