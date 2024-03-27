0ea98cef20cea03883da8a3d2b696fc133c90d0d
Queendarlin <queendarlinnnamani@gmail.com>
2024-03-27 13:19:17 -0700

    Technical

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,1 +1,17 @@
-#!/usr/bin/python3
+#!/usr/bin/ python3
+"""Function for an algorithm"""
+
+
+def find_peak(list_of_integers):
+    """
+    Find a peak in a list of unsorted integers.
+
+    Args:
+    - list_of_integers: A list of integers.
+
+    Returns:
+    - The peak integer if found, None otherwise.
+    """
+    if list_of_integers:
+        list_of_integers.sort(reverse=True)
+        return list_of_integers[0]
