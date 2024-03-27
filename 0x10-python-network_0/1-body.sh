61daae9c4864108c01ae1dc525c9c096a8fb617c
Queendarlin <queendarlinnnamani@gmail.com>
2024-03-27 11:57:33 -0700

    Task1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes a URL as an argument, sends a GET request to that URL, and displays the body of the response if the response status code is 200
-curl -s "$1" | cat
+curl -s -L "{$1}"
