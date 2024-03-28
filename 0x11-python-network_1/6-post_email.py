40abd1294f609d4ffdf4b688ebbb385610f49503
Queendarlin <queendarlinnnamani@gmail.com>
2024-03-28 11:25:51 -0700

    POST an email #1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""
+Sends a POST request to a URL with an email parameter
+and displays the body of the response.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    my_url = sys.argv[1]
+    email = sys.argv[2]
+
+    # Prepare the data to be sent in the POST request
+    data = {'email': email}
+
+    # Send a POST request with the data to the specified URL
+    response = requests.post(my_url, data=data)
+
+    # Print the body of the response
+    print(response.text)
