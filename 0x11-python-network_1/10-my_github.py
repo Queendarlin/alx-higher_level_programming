bd99ed33c4f2cd57e21ada4d4ff99a0af0729571
Queendarlin <queendarlinnnamani@gmail.com>
2024-03-28 12:29:56 -0700

    Github

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""
+Uses the GitHub API with Basic Authentication to display your GitHub user ID.
+"""
+
+import requests
+import sys
+from requests.auth import HTTPBasicAuth
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+
+    authentication = HTTPBasicAuth(username, password)
+    response = requests.get("https://api.github.com/user", auth=authentication)
+
+    user_data = response.json()
+    user_id = user_data.get("id")
+    print(user_id)
