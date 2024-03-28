8d6dfd857cbba5348918491b40a69d23f68cc6ab
Queendarlin <queendarlinnnamani@gmail.com>
2024-03-28 10:12:40 -0700

    My status #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""
+Fetches the status from https://alx-intranet.hbtn.io/status using the requests
+package. Displays the body of the response in a specific format.
+"""
+
+import requests
+
+if __name__ == "__main__":
+    my_url = "https://alx-intranet.hbtn.io/status"
+
+    response = requests.get(my_url)
+
+    print("Body response:")
+    print("\t- type:", type(response.text))
+    print("\t- content:", response.text)
