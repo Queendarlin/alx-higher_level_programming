51d4a89bd629dbdf8442ec2b85f916e4b7229847
Queendarlin <queendarlinnnamani@gmail.com>
2024-03-28 09:34:06 -0700

    POST an email #0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""
+Sends a POST request with an email parameter to a specified URL 
+and displays the body of the response (decoded in utf-8).
+"""
+
+from urllib import request, parse
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    # Encode the email parameter
+    data = parse.urlencode({'email': email}).encode('utf-8')
+
+    # Send the POST request and read the response
+    with request.urlopen(url, data=data) as response:
+        response_body = response.read().decode('utf-8')
+        print(response_body)
