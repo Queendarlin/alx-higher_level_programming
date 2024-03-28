29129f4f9431b456bfcab9c001d979566f20a9fe
Queendarlin <queendarlinnnamani@gmail.com>
2024-03-28 09:53:35 -0700

    Error code #0

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""
+Sends a request to a URL and displays the body of the response
+(decoded in utf-8).
+Handles urllib.error.HTTPError exceptions and prints the HTTP status code
+in case of an error.
+"""
+
+from urllib import request, error
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    try:
+        with request.urlopen(url) as response:
+            print(response.read().decode('utf-8'))
+    except error.HTTPError as e:
+        print(f"Error code: {e.code}")
