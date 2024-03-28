c130f774d195ec6a74e6635ac7523703a3677412
Queendarlin <queendarlinnnamani@gmail.com>
2024-03-28 12:44:03 -0700

    Github2

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python3
+"""
+A script that lists 10 commits (from the most recent to oldest) of a
+repository by a specified user using the GitHub API.
+"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    repo_name = sys.argv[1]
+    owner_name = sys.argv[2]
+
+    # GitHub API endpoint for commits
+    url = f"https://api.github.com/repos/{owner_name}/{repo_name}/commits"
+
+    # Send a GET request to the GitHub API
+    response = requests.get(url)
+
+    try:
+        commits = response.json()[:10]  # Get the 10 most recent commits
+
+        # Print each commit sha and author name
+        for commit in commits:
+            sha = commit['sha']
+            author_name = commit['commit']['author']['name']
+            print(f"{sha}: {author_name}")
+    except IndexError:
+        print("Error: Less than 10 commits available")
