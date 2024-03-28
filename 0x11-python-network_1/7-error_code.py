ac06fa1184fe655a7a825c0442d1f1af38923d76
Queendarlin <queendarlinnnamani@gmail.com>
2024-03-28 11:32:58 -0700

    Error code #1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""
+Sends a request to a URL and displays the body of the response.
+If the HTTP status code >= 400, prints an error message with the status code
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    my_url = sys.argv[1]
+
+    # Send a GET request to the specified UR
+    response = requests.get(my_url)
+
+    # Print the body of the response
+    print(response.text)
+
+    # Check if the status code is >= 400
+    if response.status_code >= 400:
+        print("Error code:", response.status_code)
