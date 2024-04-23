9ed9c3a37c6ecde3564a8798c05f44b40d4f8aa6
Queendarlin <queendarlinnnamani@gmail.com>
2024-04-23 12:19:00 -0700

    Task 7

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,1 +1,35 @@
 #!/usr/bin/node
+
+// Import the 'request' module for making HTTP requests
+const request = require('request');
+
+// Get the Movie ID from command-line argument
+const movieId = process.argv[2];
+
+// Construct the API URL with the movie ID
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+// Make a GET request to the Star Wars API endpoint
+request.get(apiUrl, (error, response, body) => {
+  if (error) {
+    // If an error occurs during the request, log the error
+    console.error(error);
+    process.exit(1);
+  }
+
+  // Parse the JSON response body
+  const movieData = JSON.parse(body);
+
+  // Print all characters of the movie
+  movieData.characters.forEach(characterUrl => {
+    request.get(characterUrl, (charError, charResponse, charBody) => {
+      if (charError) {
+        console.error(charError);
+        process.exit(1);
+      }
+
+      const characterData = JSON.parse(charBody);
+      console.log(characterData.name);
+    });
+  });
+});
