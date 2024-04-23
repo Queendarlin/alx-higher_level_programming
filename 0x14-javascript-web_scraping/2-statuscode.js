d19f963b6250e809351374f030ae4de0771a45ae
Queendarlin <queendarlinnnamani@gmail.com>
2024-04-23 11:36:24 -0700

    Status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+// Import the 'request' module for making HTTP requests
+const request = require('request');
+
+// Get the URL from command-line argument
+const url = process.argv[2];
+
+// Make a GET request to the specified URL
+request.get(url, (error, response) => {
+  if (error) {
+    // If an error occurs during the request, log the error
+    console.error(error);
+    process.exit(1);
+  }
+
+  // Log the status code of the response
+  console.log(`code: ${response.statusCode}`);
+});
