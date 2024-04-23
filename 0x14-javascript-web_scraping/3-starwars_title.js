0a2ee9f45a7a12d5a3c84b680ffc2606272d0d63
Queendarlin <queendarlinnnamani@gmail.com>
2024-04-23 11:43:56 -0700

    Star wars movie title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,31 @@
 #!/usr/bin/node
+
+// Import the 'request' module for making HTTP requests
+const request = require('request');
+
+// Get the movie ID from command-line argument
+const movieId = process.argv[2];
+
+// Construct the API URL with the movie ID
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+// Make a GET request to the Star Wars API endpoint
+request.get(apiUrl, (error, response, body) => {
+  if (error) {
+    // If an error occurs during the request, log the error
+    console.error(error);
+    process.exit(1);
+  }
+
+  // Parse the JSON response body
+  const movieData = JSON.parse(body);
+
+  // Check if the movie data contains the title
+  if (movieData.title) {
+    // Print the title of the movie
+    console.log(movieData.title);
+  } else {
+    console.error('Movie not found');
+    process.exit(1);
+  }
+});
