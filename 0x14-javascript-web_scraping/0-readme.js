b80765c9af675e2c4a3462b025e6ee9f4d3774db
Queendarlin <queendarlinnnamani@gmail.com>
2024-04-23 10:36:28 -0700

    Task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+const fs = require('fs');
+
+const filePath = process.argv[2];
+
+if (!filePath) {
+  console.error('Please provide a file path as an argument.');
+  process.exit(1);
+}
+
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+
+  console.log(data);
+});
