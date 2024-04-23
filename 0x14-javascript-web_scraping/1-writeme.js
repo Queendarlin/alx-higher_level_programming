f801033debadf7ac2bf3648dd04c89ac6287a7c9
Queendarlin <queendarlinnnamani@gmail.com>
2024-04-23 11:11:05 -0700

    Task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,20 +1,20 @@
 #!/usr/bin/node
 
-// Import the 'fs' module for file system operations
 const fs = require('fs');
 
-// Get the file path and content from command-line arguments
 const filePath = process.argv[2];
 const content = process.argv[3];
 
-// Write the content to the file asynchronously
-fs.writeFile(filePath, content, (err) => {
+if (!filePath || !content) {
+  console.error('Provide the file path and content as arguments');
+  process.exit(1);
+}
+
+fs.writeFile(filePath, content, 'utf-8', (err) => {
   if (err) {
-    // If an error occurs during writing, log the error and exit with error code 1
     console.error(err);
     process.exit(1);
   }
 
-  // If writing is successful, log a success message
   console.log(`Content written to file: ${filePath}`);
 });
